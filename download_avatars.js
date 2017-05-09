e156beefef337e10b6089a6a3a0e2e5750c75334
grantran <grant.tran@gmail.com>
2017-05-09 00:30:16 +0000

    added user-agent, also parsed body from request

--- download_avatars.js
+++ download_avatars.js
@@ -8,7 +8,24 @@ const GITHUB_TOKEN = '7147870fc9cc9e962392e12ef25d036266ee5f73'
 function getRepoContributors(repoOwner, repoName, cb) {
   const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' +
   repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+
+  const options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'GitHub Avatar Downloader - Student Project',
+  }
+  };
+
+
+  request(options, function(err, response, body) {
+    if (!err) {
+      let parsedBody = JSON.parse(body);
+      console.log(parsedBody);
+    } else {
+      console.log('error');
+      return;
+    }
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, results){
